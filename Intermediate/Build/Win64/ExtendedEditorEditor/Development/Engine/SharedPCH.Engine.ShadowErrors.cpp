b03828f98d3c978c477e794ab747f246f554609e
Steven Lim <choonwoo49@gmai.com>
2025-01-14 10:58:17 +0900

    Add project files.

---
+++ Intermediate/Build/Win64/ExtendedEditorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30157
+#include "I:/000_UnrealWork/ExtendedEditor/Intermediate/Build/Win64/ExtendedEditorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/ExtendedEditor/ExtendedEditorGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EXTENDEDEDITOR_ExtendedEditorGameModeBase_generated_h
+#error "ExtendedEditorGameModeBase.generated.h already included, missing '#pragma once' in ExtendedEditorGameModeBase.h"
+#endif
+#define EXTENDEDEDITOR_ExtendedEditorGameModeBase_generated_h
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_SPARSE_DATA
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_RPC_WRAPPERS
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAExtendedEditorGameModeBase(); \
+	friend struct Z_Construct_UClass_AExtendedEditorGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AExtendedEditorGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ExtendedEditor"), NO_API) \
+	DECLARE_SERIALIZER(AExtendedEditorGameModeBase)
+
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAExtendedEditorGameModeBase(); \
+	friend struct Z_Construct_UClass_AExtendedEditorGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AExtendedEditorGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ExtendedEditor"), NO_API) \
+	DECLARE_SERIALIZER(AExtendedEditorGameModeBase)
+
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AExtendedEditorGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AExtendedEditorGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AExtendedEditorGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AExtendedEditorGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AExtendedEditorGameModeBase(AExtendedEditorGameModeBase&&); \
+	NO_API AExtendedEditorGameModeBase(const AExtendedEditorGameModeBase&); \
+public:
+
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AExtendedEditorGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AExtendedEditorGameModeBase(AExtendedEditorGameModeBase&&); \
+	NO_API AExtendedEditorGameModeBase(const AExtendedEditorGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AExtendedEditorGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AExtendedEditorGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AExtendedEditorGameModeBase)
+
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_12_PROLOG
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_SPARSE_DATA \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_RPC_WRAPPERS \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_INCLASS \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_SPARSE_DATA \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> EXTENDEDEDITOR_API UClass* StaticClass<class AExtendedEditorGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_ExtendedEditor_Source_ExtendedEditor_ExtendedEditorGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
